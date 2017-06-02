52293633a9e4b7f805e74cada8785eff5948a5cb
chenlijun1227 <nicoleljc1227@163.com>
2017-06-02 18:40:08 +0800

    华为机试-> 题号23

---
+++ src/huawei/num_24.java
@@ -0,0 +1,30 @@
+package huawei;
+
+
+import java.util.Scanner;
+
+/**
+ * Created by xl on 2017/4/10.
+ *
+ */
+public class num_24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int N = sc.nextInt();
+            int height[] = new int[N];
+            int[] value = new int[N];
+            for (int i = 0; i < N; i++) {
+                height[i] = sc.nextInt();
+            }
+            for (int i = 0; i <N ; i++) {
+
+            }
+
+
+
+
+
+        }
+    }
+}
